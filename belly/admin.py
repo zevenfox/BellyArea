e39c36dccc7ffd2c5252f6d9134174b15f866724
Kollawat Rupanya <64556398+zevenfox@users.noreply.github.com>
2022-12-16 15:00:02 +0700

    Delete belly directory

--- belly/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Menu, TopPick
-# Register your models here.
-admin.site.site_header = "BellyArea Admin"
-admin.site.site_title = "Admin Area"
-admin.site.index_title = "Welcome to the BellyArea Admin Area"
-
-admin.site.register(Menu)
-admin.site.register(TopPick)
--- belly/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class BellyConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'belly'
